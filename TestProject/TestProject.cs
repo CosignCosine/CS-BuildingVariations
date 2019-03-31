28e6a0897efba87109add7173a206a96a431af8d
Scott Schraeder <CosignCosine@users.noreply.github.com>
2019-03-31 18:00:16 -0500

    Add files via upload

---
+++ TestProject/TestProject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ICities;
+using Harmony;
+using System.Reflection;
+
+namespace TestProject
+{
+    public class TestProject : IUserMod
+    {
+        private readonly string harmonyId = "elektrix.buildingvariations";
+        private HarmonyInstance harmony;
+
+        public string Name => "! Elektrix's Test Mod";
+        public string Description => "Where elektrix tests his electric things. [in a purely platonic manner]";
+
+        public void OnEnabled(){
+            harmony = HarmonyInstance.Create(harmonyId);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void OnDisabled(){
+            harmony.UnpatchAll(harmonyId);
+            harmony = null;
+        }
+    }
+}
---
+++ TestProject/TestProjectBuildingData.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace TestProject
+{
+    public class BuildingVariation {
+        
+        public string m_publicName;
+
+        public List<string> m_enabledSubMeshes;
+        public List<string> m_enabledSubBuildings;
+        public List<string> m_enabledOtherBuildings;
+
+        public bool IsVariationEnabled(ref Building data){
+            List<BuildingInfo.MeshInfo> makeVisibleOnly = data.Info.m_subMeshes.Where(r => m_enabledSubMeshes.Contains(r.m_subInfo.name)).ToList();
+            List<BuildingInfo.MeshInfo> makeHidden = data.Info.m_subMeshes.Where(r => !m_enabledSubMeshes.Contains(r.m_subInfo.name)).ToList();
+
+            for (int i = 0; i < makeHidden.Count; i++){
+                BuildingInfo.MeshInfo info = makeHidden[i];
+
+                if((info.m_flagsForbidden & Building.Flags.Created) == Building.Flags.None){
+                    return false;
+                }
+            }
+
+            for (int i = 0; i < makeVisibleOnly.Count; i++)
+            {
+                BuildingInfo.MeshInfo info = makeVisibleOnly[i];
+
+                if ((info.m_flagsForbidden & Building.Flags.Created) != Building.Flags.None)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void ApplyVariation(ref Building data){
+            List<BuildingInfo.MeshInfo> makeVisibleOnly = data.Info.m_subMeshes.Where(r => m_enabledSubMeshes.Contains(r.m_subInfo.name)).ToList();
+            List<BuildingInfo.MeshInfo> makeHidden = data.Info.m_subMeshes.Where(r => !m_enabledSubMeshes.Contains(r.m_subInfo.name)).ToList();
+
+            for (int i = 0; i < makeHidden.Count; i++){
+                BuildingInfo.MeshInfo info = makeHidden[i];
+                if (info != null && info.m_subInfo != null && (info.m_flagsForbidden & Building.Flags.Created) == Building.Flags.None){
+                    info.m_flagsForbidden |= Building.Flags.Created;
+                } 
+            }
+
+            for (int i = 0; i < makeVisibleOnly.Count; i++)
+            {
+                BuildingInfo.MeshInfo info = makeVisibleOnly[i];
+                if (info != null && info.m_subInfo != null && (info.m_flagsForbidden & Building.Flags.Created) != Building.Flags.None){
+                    info.m_flagsForbidden &= ~Building.Flags.Created;
+                } 
+            }
+        }
+    }
+
+    public static class TestProjectBuildingData
+    {
+        public static Dictionary<string, List<BuildingVariation>> NameToVariationDataMap = new Dictionary<string, List<BuildingVariation>>();
+        public static bool deserialized = false;
+
+        public static void Deserialize(){
+            if (!deserialized)
+            {
+                deserialized = true;
+                BuildingVariation testVariation = new BuildingVariation();
+                testVariation.m_publicName = "Orange Cube";
+                testVariation.m_enabledSubMeshes = new List<string>();
+                testVariation.m_enabledSubMeshes.Add("cube1");
+
+                BuildingVariation testVariation2 = new BuildingVariation();
+                testVariation2.m_publicName = "Green and Yellow Cubes";
+                testVariation2.m_enabledSubMeshes = new List<string>();
+                testVariation2.m_enabledSubMeshes.Add("cube2");
+                testVariation2.m_enabledSubMeshes.Add("cube3");
+
+                List<BuildingVariation> r = new List<BuildingVariation>();
+                r.Add(testVariation);
+                r.Add(testVariation2);
+                NameToVariationDataMap.Add("ELEKTRIX TEST ASSET", r);
+            }
+        }
+    }
+}
